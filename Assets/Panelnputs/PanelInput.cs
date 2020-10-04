2021fa44ced1ce526d3e37f9c52b952119291ba9
Claudio@DESKTOP-814V7UC.lan <klav91@gmail.com>
2020-10-04 17:53:18 +0200

    panel inputs

---
+++ Assets/Panelnputs/PanelInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
+        {
+            GetComponent<Animator>().enabled = true;
+            StartCoroutine(DisableMe());
+        }
+    }
+
+    IEnumerator DisableMe()
+    {
+        yield return new WaitForSeconds(1.7f);
+        this.gameObject.SetActive(false);
+    }
+}
